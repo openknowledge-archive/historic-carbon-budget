ea18d809ac32d9f502ada58584eafc7adcbf7b93
Tom Rees <zephod@gmail.com>
2012-03-03 17:21:43 +0000

    [highcharts-merge]: Rendering emissions-per-capita from ElasticSearch

--- carbon-budget.js
+++ carbon-budget.js
@@ -15,9 +15,46 @@ $(function() {
 
 
 function processData(data) {
+  var xAxis = [];
+  var yAxis = [];
   $.each(data.hits.hits, function(index, hit) {
-
     console.log(hit.fields['Year'] + ' :: ' + hit.fields['CO2 emissions (metric tons per capita)']);
+    var yearString = hit.fields['Year'];
+    var year = parseInt(yearString);
+    if (year>2008) return;
+    xAxis.push(year);
+    var string = hit.fields['CO2 emissions (metric tons per capita)'];
+    var n = parseFloat(string || '0');
+    yAxis.push(n);
   });
+  console.log(yAxis);
 
+  var myChart = new Highcharts.Chart({
+    chart: {
+      renderTo: 'chart',
+      type: 'spline'
+    },
+    title: {
+      text: 'CO2 emissions (metric tons per capita)'
+    },
+    xAxis: {
+      categories: xAxis,
+      labels: {
+        step: 10
+      }
+    },
+    yAxis: {
+      minPadding: 0.2,
+      maxPadding: 0.2,
+      title: {
+        text: 'CO2/GDP',
+        margin: 20
+      }
+    },
+    series: [{
+      name: 'Some data',
+      data: yAxis
+    }]
+  });
 }
+
